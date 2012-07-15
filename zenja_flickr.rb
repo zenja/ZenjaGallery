d561726880dd68aa0a5c57b720ec7cf57c6ef8b4
Xing, Wang <wang.xing.me@gmail.com>
2012-07-15 12:47:37 +0800

    basic flickr function

--- zenja_flickr.rb
+++ zenja_flickr.rb
@@ -1,4 +1,60 @@
 require 'flickraw'
+require 'yaml'
 
-FlickRaw.api_key = "e9190ae28510a3bc2a781bd502b9f0c7"
-FlickRaw.shared_secret = "8a94a0e87194f5a2"
+class ZenjaFlickr
+  @@flickr_yaml = YAML.load(File.open("./config/flickr.yaml"))
+  @@zenja_id = '78766733@N06'
+
+  def ZenjaFlickr.init
+    FlickRaw.api_key = @@flickr_yaml['app']['api_key']
+    FlickRaw.shared_secret = @@flickr_yaml['app']['shared_secret']
+  end
+
+  def photosets
+    flickr.photosets.getList :user_id => @@zenja_id
+  end
+
+  def photos_in_set(set_id)
+    flickr.photosets.getPhotos  :photoset_id => set_id, 
+                                :privacy_filter => 1, 
+                                :media => 'photos', 
+                                :extras => 'date_taken,url_sq'
+  end
+
+  def url_s(info)
+    FlickRaw.url_s info
+  end
+
+  def url(info)
+    FlickRaw.url info
+  end
+
+  def url_c(info)
+    FlickRaw.url_c info
+  end
+
+  def test
+    info = flickr.photos.getInfo :photo_id => '7559507782'
+    list = flickr.photosets.getList :user_id => @@zenja_id
+
+    puts list.class
+    p list
+    puts info.title
+    puts info.dates.taken
+    puts FlickRaw.url_photosets(info)
+    #puts list
+  end
+end
+
+ZenjaFlickr.init
+
+=begin
+zf = ZenjaFlickr.new
+#zf.test
+list = zf.photos_in_set(zf.photosets[0].id)
+puts list.class
+p list
+puts zf.url_s(list['photo'][0])
+puts zf.url_c(list['photo'][0])
+puts zf.url(list['photo'][0])
+=end
